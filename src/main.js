b86b9a486b136373217faa5b5918ad70a0fcd055
Romashine <miroshin.ry@gmail.com>
2017-06-18 11:47:41 +0300

    Добавлены основные операции

---
+++ src/main.js
@@ -0,0 +1,6 @@
+console.log("Before function")
+function test() {
+    //alert("Hello world");
+    console.log("Inner function")
+}
+console.log("After function")
---
+++ src/person.js
@@ -0,0 +1,67 @@
+/// <reference path="./main.js" />
+/**
+ * 
+ * @param {*} name 
+ * @param {*} age 
+ */
+function personCreate(name, age) {
+    var person = {
+        name: name,
+        age: age,
+    };
+    return person;
+}
+var person1 = personCreate("Bob", 1)
+console.log("Person", person1)
+
+// one commet
+
+/* multi comment
+*/
+
+//if
+if (person1 != null) {
+
+} else {
+
+}
+/**
+ * else if
+ * && and
+ * || or
+ * > < == === != !== >= <=
+ */
+
+switch (n) {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+}
+
+for (var i = 0; i < array1.length; i++) {
+    var item = array1[i];
+    console.log("Item", i, item);
+}
+
+var i = 0;
+while (i < array1.length) {
+    var item = array1[i];
+    i++;
+}
+
+n = n + 1;
+n += 1;
+n++;
+++n;
+
+str += "!!!"
+
+
+var str = "Bob";
+var num = 1;
+var float = 2.4;
+var b = true;
+var array = [1, 2, 3, 4, 5, 6];
+var array1 = ["sads", true, -1234, person1];
